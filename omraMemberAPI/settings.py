43504a363ace1f06874616eeeea7907e617a4b8e
Unitedideas <unitedideas@gmail.com>
2018-12-10 21:50:20 -0800

    settings changed to read only if authenticated Need to add import/ export for init database Need to look into the workflow of the person managing the data and add functionality

--- omraMemberAPI/settings.py
+++ omraMemberAPI/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
