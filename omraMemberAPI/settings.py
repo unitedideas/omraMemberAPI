c982431c51619108c4c31c027cbc53f1c0be4159
Unitedideas <unitedideas@gmail.com>
2018-12-10 21:46:41 -0800

    settings changed to read only if authenticated Need to add import/ export for init database Need to look into the workflow of the person managing the data and add functionality

--- omraMemberAPI/settings.py
+++ omraMemberAPI/settings.py
@@ -28,7 +28,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['omraapi.pythonanywhere.com']
 
 # Application definition
 
