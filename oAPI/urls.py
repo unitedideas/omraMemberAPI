54339cf6573d32cb881b100cb9cc52747095934f
Unitedideas <unitedideas@gmail.com>
2018-12-19 19:39:39 -0800

    added an init member page

--- oAPI/urls.py
+++ oAPI/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
 router.register('riderdata', views.RiderView)
 urlpatterns = [
